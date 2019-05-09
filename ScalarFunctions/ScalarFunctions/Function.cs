19babf7f1c54e172b46af7bec1ec07b48e40f2be
MohamedAdel <mohamed.adel90211@gmail.com>
2019-05-09 23:56:14 +0200

    Created class functions, Avg

--- ScalarFunctions/ScalarFunctions/Function.cs
+++ ScalarFunctions/ScalarFunctions/Function.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace ScalarFunctions
 {
@@ -28,5 +31,59 @@ namespace ScalarFunctions
                 dataTypes[i] = dataType;
             }
         }
+    }
+
+    public class Functions
+    {
+        private List<Table> tables;
+        private Column column; 
+        
+        public Functions(string tableName, string columnHeader)
+        {
+            FileStream file = new FileStream("Tables.xml", FileMode.Open);
+            XmlSerializer ser = new XmlSerializer(tables.GetType());
+            tables = (List<Table>)ser.Deserialize(file);
+
+            for(int i = 0; i < tables.Count; i++)
+            {
+                Table table = tables[i];
+                if(table.name == tableName)
+                {
+                   for(int j = 0; j < table.columns.Count; j++)
+                    {
+                        Column column = table.columns[j];
+                        if (column.header == columnHeader) {
+                            this.column = column;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public double Avg()
+        {
+            double answer = 0.0;
+            int count = column.values.Count;
+            for (int i = 0; i < column.values.Count; i++)
+            {
+                   if (column.dataType == "double")
+                    {
+                        double sum = 0;
+                        double number = double.Parse(column.values[i]);
+                        sum += number;
+                        answer =  (sum / count);
+                    }
+                    else
+                    {
+                        int sum = 0;
+                        int number = int.Parse(column.values[i]);
+                        sum += number;
+                        answer = (sum / count);
+                     }
+                }
+            return answer;
+        }
+           
     }
 }
